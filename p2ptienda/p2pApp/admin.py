5fe0275c9d956fccda48c90019b3fa372812962f
dia230983 <57355177+dia230983@users.noreply.github.com>
2020-03-04 13:19:38 +0100

    actualizado diana

--- p2ptienda/p2pApp/admin.py
+++ p2ptienda/p2pApp/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Producto, Categoria, Usuario
+from .models import Producto, Categoria
 
 
 
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Categoria)
-admin.site.register(Usuario)
--- p2ptienda/p2pApp/views.py
+++ p2ptienda/p2pApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View,ListView,TemplateView,CreateView
 from django.urls import reverse_lazy
-from .forms import ProForm
-from .models import Categoria, Producto, Usuario
+from .forms import ProForm, CarroForm
+from .models import Categoria, Producto, Carrito
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
 class index(TemplateView):
     template_name = 'p2pApp/index.html'
 
@@ -13,3 +16,33 @@ class Consultar_Producto(ListView):
     template_name = 'p2pApp/consultaTienda.html'
     context_object_name= 'p2pApp'
     queryset = Producto.objects.all()
+
+class Consultar_Carrito(ListView):
+    model = Carrito
+    template_name = 'p2pApp/consultarCarrito.html'
+    context_object_name= 'p2pApp'
+    queryset = Carrito.objects.all()   
+
+class Carrazo(CreateView):
+    model= Carrito
+    template_name = 'p2pApp/cestaProduct.html'
+    form_class = CarroForm
+    success_url = reverse_lazy('p2pApp:consultar_producto')
+
+    def form_valid(self,form):
+        self.object = form.save(commit=False)
+        producto = Producto.objects.get(pk = self.kwargs.get('pk',None))
+        self.object.producto = producto
+        usuario = User.objects.get(pk = 1)
+        self.object.user = usuario
+
+        self.object.save()
+        return super(Carrazo,self).form_valid(form)
+
+
+
+                
+
+
+    
+
